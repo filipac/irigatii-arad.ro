fb7bf1be7fcacee9c33599ea9fa23b77ca675103
Filip Pacurar <filip@pacurar.net>
2021-01-30 07:14:09 +0200

    top bar

--- components/TopBar.js
+++ components/TopBar.js
@@ -4,7 +4,7 @@ export const TopBar = () => {
   return (
     <div tw="bg-brown-900  p-4">
       <div tw="container mx-auto w-full bg-brown-900 flex">
-        <div tw="text-white flex flex-row">
+        <div tw="text-xs md:text-base text-white flex flex-row">
           <div>
             <span tw="font-bold">Telefon:</span> 0721 123 456
           </div>
