904e9589c71a3427e4359f8dc339f1db5f8bbe1a
Filip Pacurar <filip@pacurar.net>
2021-01-30 07:12:53 +0200

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,10 @@ import tw from 'twin.macro';
 export const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div tw="mt-16 w-full bg-brown-900 py-8 flex justify-center text-white">
+    <div tw="mt-16 w-full bg-brown-900 py-8 px-8 md:px-0 flex flex-col text-center items-center justify-center text-white">
+      <div>Copyright &copy; {year} Irigatii Arad.</div>
       <div>
-        Copyright &copy; {year} Irigatii Arad. Freshly backed by{' '}
+        Freshly backed by{' '}
         <a
           href="https://pacurar.net"
           target="_blank"
